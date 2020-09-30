36212f68a26a1d7016411db3e8a5ba77f32d5620
DeniiseB <denisebradley92@hotmail.com>
2020-09-30 22:42:24 +0200

    Improved changes in Game to add players to arraylist with addPlayer() method

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -7,7 +7,7 @@ public class Game {
 
     Scanner scanner = new Scanner(System.in);
 
-    public ArrayList<Player> players = new ArrayList<>();;
+    public ArrayList<Player> players = new ArrayList<>();
     public int rounds;
     public String newPlayer = "";
 
@@ -15,29 +15,30 @@ public class Game {
         System.out.println("Choose rounds (1-30)");
         this.rounds = scanner.nextInt();
 
-        while(!newPlayer.equals("0")){      // Add limit to players (4)
-        System.out.println("Enter name of player (0 to skip to Main Menu):");
-        newPlayer = scanner.next();
-        if(newPlayer.equals("0")){continue;}
-        addPlayer(newPlayer);
-        }
+        addPlayer();
 
         System.out.println("Main Menu");
-        for(Player player: players){
+        for (Player player : players) {
             System.out.println(player.getName());
         }
 
     }
 
-    public void addPlayer(String playerName) {
-        players.add(new Player(playerName));
+    public void addPlayer() {
+
+            System.out.println("Enter name of player (0 to skip to Main Menu):");
+            newPlayer = scanner.next();
+            if (newPlayer.equals("0")) { return;}
+            this.players.add(new Player(newPlayer));
+            if (players.size() > 3){ return; }
+
+            addPlayer();
     }
 
-
-    public void menu() {
+//    public void menu() {
+//
+//
+//    }
 
 
     }
-
-
-}
