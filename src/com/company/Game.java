3e8ffee9adc97456da03b502a20217f98f2bab93
DeniiseB <denisebradley92@hotmail.com>
2020-10-01 11:58:07 +0200

    Added a Main Menu with 3 options (rules, start game and exit). Added a block in addPlayer() to ensure at least 1 player gets added before game start.

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -12,33 +12,59 @@ public class Game {
     public String newPlayer = "";
 
     public Game() {
-        System.out.println("Choose rounds (1-30)");
+        System.out.println("Choose rounds (5-30)");
         this.rounds = scanner.nextInt();
 
         addPlayer();
 
-        System.out.println("Main Menu");
-        for (Player player : players) {
-            System.out.println(player.getName());
-        }
+        mainMenu();
+
 
     }
 
     public void addPlayer() {
+        // Method that adds players to players arraylist
+        System.out.println("Enter name of player (0 to skip to Main Menu):");
+        newPlayer = scanner.next();
 
-            System.out.println("Enter name of player (0 to skip to Main Menu):");
-            newPlayer = scanner.next();
-            if (newPlayer.equals("0")) { return;}
-            this.players.add(new Player(newPlayer));
-            if (players.size() > 3){ return; }
+        if (newPlayer.equals("0")) {
+            // Making sure at least one player gets added
+            if (players.size() == 0) {
+                System.out.println("You must add at least one player!");
+                addPlayer();
+            }
+            return;
+        }
 
-            addPlayer();
+        this.players.add(new Player(newPlayer));
+        if (players.size() > 3) {
+            return;
+        }
+
+        addPlayer();
     }
 
-//    public void menu() {
-//
-//
-//    }
+    public void mainMenu() {
 
+        System.out.println("\nMAIN MENU\n---------\n(Pick 1-3, press ENTER)");
+        System.out.println("1. Game rules\n2. Start Game\n3. Exit game");
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.println("Game rules:\n You have picked " + rounds + " rounds...ADD MORE.\n");
+                mainMenu();
+            case 2:
+                System.out.println("Start game method");
+                break;
+            case 3:
+                System.exit(0);
+
+        }
+    }
+
+    public void gameMenu() {
 
     }
+
+
+}
