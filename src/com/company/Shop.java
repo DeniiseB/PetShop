2385254a37f9530fddc8b15f3daf925de90bffd3
DeniiseB <denisebradley92@hotmail.com>
2020-10-04 12:41:43 +0200

    Made takingPayment() method work in Shop

--- src/com/company/Shop.java
+++ src/com/company/Shop.java
@@ -7,23 +7,22 @@ public class Shop {
     public void buyAnimal(Player player) {
         int choice = 0;
         boolean loop = true;
-        do{
-        Scanner scanner = new Scanner(System.in);
-        print("\n".repeat(50) + "\nThe Dodgy Pet Shop\n------------------\n(Select 1-6. Press ENTER)");
-        print("1. Toad, £5 \n2. Pike, £10 \n3. Pheasant, £20 \n4. Ferret, £40" +
-                " \n5. Badger, £60");
-        try{
-        choice = scanner.nextInt();
-        loop = false;
-        }
-        catch (Exception e){
-            System.out.println("Only type numbers 1 - 5!");
-        }
-        if (choice < 1 || choice > 5) {
-            print("Only type numbers 1 - 5!");
-            loop = true;
-        }
-        }while(loop);
+        do {
+            Scanner scanner = new Scanner(System.in);
+            print("\n".repeat(50) + "\nThe Dodgy Pet Shop\n------------------\n(Select 1-6. Press ENTER)");
+            print("1. Toad, £5 \n2. Pike, £10 \n3. Pheasant, £20 \n4. Ferret, £40" +
+                    " \n5. Badger, £60");
+            try {
+                choice = scanner.nextInt();
+                loop = false;
+            } catch (Exception e) {
+                System.out.println("Only type numbers 1 - 5!");
+            }
+            if (choice < 1 || choice > 5) {
+                print("Only type numbers 1 - 5!");
+                loop = true;
+            }
+        } while (loop);
         addAnimal(player, choice);
 
         return;
@@ -53,11 +52,21 @@ public class Shop {
             case 5 -> new Badger(petName, gender);
             default -> throw new IllegalStateException("Unexpected value");
         };
-        player.animals.add(newAnimal);
+        // If player has enough money to buy animal, takingPayment returns true
+        if (takingPayment(player, newAnimal)) {
+            player.animals.add(newAnimal);
+        }
     }
 
-    public void takingPayment(Player player, Animal animal){
+    public boolean takingPayment(Player player, Animal animal) {
+        if (player.getMoney() < animal.getPrice()) {
+            print("You don't have enough money to buy that pet. You have £" + player.getMoney() + " left.");
+            return false;
+        }
         player.setMoney(player.getMoney() - animal.getPrice());
+        print("Hope you enjoy your " + animal.getClass().getSimpleName() + "!\nYou have £"
+                + player.getMoney() + " left.");
+        return true;
     }
 
     public void sellAnimal() {
